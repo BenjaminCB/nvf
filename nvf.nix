57fbaab25631a3a198c34294c0ddb4e20b6f2947
bcb <benjamincb@hotmail.dk>
2025-08-09 13:53:54 +0200

    add options

--- nvf.nix
+++ nvf.nix
@@ -1,6 +1,36 @@
 { pkgs, ... }: 
 {
   config.vim = {
+    globals.mapleader = " ";
+    lineNumberMode = "relNumber";
+
+    options = {
+      wrap = false;
+      colorcolumn = "80";
+      shiftwidth = 4;
+      tabstop = 4;
+      expandtab = true;
+      smartindent = true;
+      splitright = true;
+      splitbelow = true;
+      softtabstop = 4;
+      wildmode = "longest,list,full";
+      ruler = true;
+      undolevels = 1000;
+      foldmethod = "marker";
+      exrc = true;
+      hlsearch = false;
+      scrolloff = 6;
+      backup = false;
+      writebackup = false;
+      updatetime = 100;
+      signcolumn = "yes";
+      termguicolors = true;
+      completeopt = "menuone,noinsert,noselect";
+      shortmess = "c";
+      cmdheight = 2;
+    };
+
     # Enable custom theming options
     theme = {
       enable = true;
